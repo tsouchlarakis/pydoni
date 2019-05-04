4a60bacafe7fd20efa7b8db8a6d3ba51cff32c6c
tsouchlarakis <Iloveandoni$44>
2019-05-04 14:37:14 -0700

    cleaned docstrings

--- pydoni/web.py
+++ pydoni/web.py
@@ -1,44 +1,67 @@
-def CheckNetworkConnection(stopifnot=False):  # Check if connected to internet
+def check_network_connection(abort=False):
+    """
+    Check if connected to internet
+    Args
+        abort (bool): if True, quit program
+    Returns
+        bool
+    """
     import urllib.request
     try:
         urllib.request.urlopen('https://www.google.com')
         return True
     except:
-        if stopifnot:
-            exit()
+        if abort:
+            quit()
         else:
             return False
 
-def getElementBySelector(url, selector, attr=None):  # Defaults to extracting HTML text
+
+def get_element_by_selector(url, selector, attr=None):
+    """
+    Extract HTML text by CSS selector.
+    Args
+        url      (str): target URL to scrape
+        selector (str): CSS selector
+        attr     (str): name of attribute to extract
+    Returns
+        str
+    """
     import requests
     from bs4 import BeautifulSoup
-    try:
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        if attr:
-            return [soup.select(selector)[i].attrs[attr] for i in range(len(soup.select(selector)))]
-        elem = [soup.select(selector)[i].text for i in range(len(soup.select(selector)))]
-        return elem
-    except Exception as e:
-        from pydoni.vb import echo
-        echo("Unable to parse CSS Selector {} for element {}".format(
-            clickfmt(url, fmt='url'), clickfmt(selector, fmt='red')), str(e),
-            fn_name='getElementBySelector', error=True)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    if attr:
+        return [soup.select(selector)[i].attrs[attr] for i in range(len(soup.select(selector)))]
+    elem = [soup.select(selector)[i].text for i in range(len(soup.select(selector)))]
+    return elem
+    
 
-def getElementByXPath(url, xpath):
+def get_element_by_xpath(url, xpath):
+    """
+    Extract HTML text by Xpath selector.
+    Args
+        url      (str): target URL to scrape
+        selector (str): CSS selector
+        attr     (str): name of attribute to extract
+    Returns
+        str
+    """
     import requests
     from lxml import html
-    try:
-        page = requests.get(url)
-        tree = html.fromstring(page.content)
-        return tree.xpath(xpath)
-    except Exception as e:
-        from pydoni.vb import echo
-        echo("Unable to parse XPath {} for element {}".format(
-            clickfmt(url, fmt='url'), clickfmt(selector, fmt='red')), str(e),
-            fn_name='getElementByXPath', error=True)
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    return tree.xpath(xpath)
 
 def downloadfile(url, destfile):
+    """
+    Download file from the web to a local file.
+    Args
+        url (str): target URL to retrieve file from
+        destfile (str): 
+    Returns
+        str
+    """
     import requests, shutil
     r = requests.get(url, stream=True)
     with open(destfile, 'wb') as f:
