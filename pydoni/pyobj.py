dcf0af0ff415c639e6a2f53022f4f4782469c3b1
tsouchlarakis <Iloveandoni$44>
2019-05-04 11:21:14 -0700

    Cleaned all docstrings

--- pydoni/pyobj.py
+++ pydoni/pyobj.py
@@ -1,4 +1,12 @@
-def dictSApply(func, d):  # Apply function to each terminal element of a dictionary
+def dictSApply(func, d):
+    """
+    Apply function to each terminal element of a dictionary.
+    Args
+        func (function): name of function to apply.
+        d    (dict)    : dictionary to apply `func` to.
+    Returns
+        dict
+    """
     if (isinstance(d, str)):
         return func(d)
     elif (isinstance(d, dict)):
@@ -6,7 +14,12 @@ def dictSApply(func, d):  # Apply function to each terminal element of a diction
     elif (isinstance(d, list)):
         return [ dictSApply(func, val) for val in d ]
 
-class DoniDict(dict):  # Dictionary class with recursive 'get' method
+class DoniDict(dict):
+    """
+    Dictionary class with recursive 'get' method.
+    Args
+        mydict (dict): dictionary to convert to DoniDict
+    """
     def __init__(self, mydict):
         import os
         self.mydict = dictSApply(os.path.expanduser, mydict)
@@ -18,25 +31,46 @@ class DoniDict(dict):  # Dictionary class with recursive 'get' method
             cursor = cursor.get(a, default)
         return cursor
 
+
 def systime(stripchars=False):
-    # Print the current time formatted as a string
+    """
+    Print the current time formatted as a string.
+    Args
+        stripchars (bool): If True, strip dashes and colons from datetime string (YYYYMMDD_HHMMSS). If False, return as YYYY-MM-DD HH:MM:SS.
+    Returns
+        str
+    """
     import datetime
     if stripchars:
-        now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        return datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     else:
-        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return now
+        return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 
 def sysdate(stripchars=False):
-    # Print the current date formatted as a string
+    """
+    Print the current date formatted as a string.
+    Args
+        stripchars (bool): If True, strip dashes and colons from date string (YYYYMMDD). If False, return as YYYY-MM-DD.
+    Returns
+        str
+    """
     import datetime
     if stripchars:
-        now = datetime.datetime.now().strftime("%Y%m%d")
+        return datetime.datetime.now().strftime("%Y%m%d")
     else:
-        now = datetime.datetime.now().strftime("%Y-%m-%d")
-    return now
+        return datetime.datetime.now().strftime("%Y-%m-%d")
 
-def assert_len(varlist, varnames):  # Ensure lengths of variables are equal, otherwise error
+
+def assert_len(varlist, varnames):
+    """
+    Ensure lengths of variables are equal, otherwise throw an error
+    Args
+        varlist (list): list of lists to test the lengths of
+        varnames (list): list of names of variables (used for verbose output)
+    Returns
+        nothing
+    """
     import click
     from pydoni.vb import echo
     lengths = [len(x) for x in varlist]
@@ -46,48 +80,89 @@ def assert_len(varlist, varnames):  # Ensure lengths of variables are equal, oth
             ', '.join("'" + click.style(str(item), fg='red', bold=True) + "'" for item in lengths)),
         fn_name='assert_len', abort=True)
 
-def userSelectFromList(lst, indent=0, msg=None, allow_range=True):
+
+def user_select_from_list(lst, indent=0, msg='Please make a selection (hyphen-separated range ok): ', allow_range=True):
+    """
+    Prompt user to make a selection from a list.
+    Args
+        lst (list): list of selections
+        indent (int): indentation level of all items of `lst`
+        msg (str): [optional] custom message to print instead of default
+        allow_range (bool): if True, allow user to make multiple selections
+    Returns
+        slice of list
+    """
     import re
     from pydoni.vb import echo
-    tab = '\t' * indent
-    if not msg:
-        msg = 'Please make a selection (hyphen-separated range ok): ' \
-            if allow_range else 'Please make a single selection: '
-    for i, item in enumerate(lst):
-        print('{}({}) {}'.format(tab, str(i+1), item))
+    
+    # Add indent to each element of `lst`
+    if indent > 0:
+        tab = '\t' * indent
+        for i, item in enumerate(lst):
+            print('{}({}) {}'.format(tab, str(i+1), item))
+    
+    # User must make a valid selection. If selection is invalid, re-run through this while loop.
     invalid = True
     while invalid:
+        # Get user input
         uin = input(msg + ': ' if not msg.rstrip().endswith(':') else msg)
+        
+        # Check if user input is valid
         if re.search(r'^(\d+-\d+)$', uin):
+            # User input is range (valid)
             if allow_range:
+                # Parse range input
                 uin = uin.split('-')
                 uin = list(range(int(uin[0]), int(uin[1])+1))
+
+                # Test if range is truly within the length of `lst`
                 if uin[len(uin)-1] > len(lst) or uin[0] < 1:
+                    # Range is outside of true length of `lst`
                     echo('Entry must be between {} and {}'.format('1', str(len(lst))), error=True)
                     invalid = True
                     continue
+                
                 else:
+                    # Range is valid, slice list at selection
                     val = [lst[i-1] for i in uin]
                     break
             else:
+                # Range is entered, but allow_range is false (invalid)
                 echo("Range entered but parameter 'allow_range' is False", error=True, abort=False)
                 invalid = True
                 continue
+        
         elif re.search(r'^(\d+)$', uin):
+            # User input is single selection (valid)
             uin = int(uin)
             if uin < 1 or uin > len(lst):
                 echo('Entry must be between {} and {}'.format('1', str(len(lst))), error=True)
                 invalid = True
                 continue
             return lst[uin-1]
+        
         else:
+            # User input is invalid (not a single selection or a range selection)
             echo("Invalid entry. Must match either '\d+' or '\d+-\d+'", error=True)
             invalid = True
             continue
+    
     return val
 
+
 def fmt_seconds(time_in_sec, units='auto', round_digits=4):
-    """Format time in seconds to a custom string"""
+    """
+    Format time in seconds to a custom string.
+    Args
+        time_in_sec  (int): time in seconds to format
+        units        (str): target units to format seconds as, one of ['auto', 'seconds', 'minutes', 'hours', 'days']
+        round_digits (int): number of digits to round to
+    Return
+        dict(
+            units = <str>,
+            value = <int>
+        )
+    """
     if units == 'auto':
         if time_in_sec < 60:
             time_diff = round(time_in_sec, round_digits)
@@ -101,6 +176,7 @@ def fmt_seconds(time_in_sec, units='auto', round_digits=4):
         else:
             time_diff = round(time_in_sec/86400, round_digits)
             time_measure = 'days'
+
     elif units in ['seconds', 'minutes', 'hours', 'days']:
         time_measure = units
         if units == 'seconds':
@@ -111,32 +187,78 @@ def fmt_seconds(time_in_sec, units='auto', round_digits=4):
             time_diff = round(time_in_sec/3600, round_digits)
         else:  # Days
             time_diff = round(time_in_sec/86400, round_digits)
+
     else:
         print("Invalid 'units' parameter. Must be one of 'auto', 'seconds', 'minutes', 'hours' or 'days'")
         return None
+
     return dict(zip(['units', 'value'], [time_measure, time_diff]))
 
-def listmode(lst):  # Get the most frequently-occurring value in a list
+
+def listmode(lst):
+    """
+    Get the most frequently-occurring value in a list.
+    Args
+        lst (list): list to get mode from
+    Returns
+        str or int
+    """
     return max(set(lst), key=lst.count)
 
+
 def dict_filter(d, keys):
+    """
+    Filter dictionary by list of keys.
+    Args
+        d    (dict): dictionary to filter
+        keys (list): key names to filter on
+    """
     return {k.lower().replace(' ', '_'): v for k, v in d.items() if k.lower().replace(' ', '_') in keys}
 
-def capNthChar(string, n):
-    """Capitalize the Nth character of a string. If 'n' is out of range, return original string"""
+
+def cap_nth_char(string, n):
+    """
+    Capitalize the Nth character of a string. If 'n' is out of range, return original string.
+    Args
+        string (str): string to consider
+        n      (int): position to capitalize letter in `string`
+    Return
+        str
+    """
     if n >= len(string):
         return string
     return string[:n] + string[n].capitalize() + string[n+1:]
 
-def replaceNthChar(string, n, replacement):
-    if n >= len(string):
-        return string
-    return string[:n] + replacement + string[n+1:]
 
-def insertNthChar(string, n, char):
+def replace_nth_char(string, n, replacement):
+    """
+    Capitalize the Nth character of a string. If 'n' is out of range, return original string.
+    Args
+        string      (str)       : string to consider
+        n           (int)       : position to capitalize letter in `string`
+        replacement (str or int): string or integer to replace nth char with
+    Return
+        str
+    """
     if n >= len(string):
         return string
-    return string [:n] + char + string[n:]
+    return string[:n] + str(replacement) + string[n+1:]
+
+
+def insert_nth_char(string, n, char):
+    """
+    Capitalize the Nth character of a string. If 'n' is out of range, return original string.
+    Args
+        string (str)       : string to consider
+        n      (int)       : position to capitalize letter in `string`
+        char   (str or int): string or integer to insert at nth position
+    Return
+        str
+    """
+    if n >= len(string):
+        return string
+    return string [:n] + str(char) + string[n:]
+
 
 def human_filesize(nbytes: int) -> str:
     """Convert number of bytes to human-readable filesize string"""
