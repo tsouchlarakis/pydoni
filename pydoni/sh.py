8b6c45043264d89c56b2e12e0286e9a45dba770b
tsouchlarakis <Iloveandoni$44>
2019-05-09 23:36:08 -0700

    moved exiftool entirely to EXIF class

--- pydoni/sh.py
+++ pydoni/sh.py
@@ -27,56 +27,6 @@ def syscmd(cmd, encoding=''):
     return p.returncode
 
 
-def exiftool(fpath, rmtags=None, attr_name=None):
-    """
-    Run `exiftool` on a file and fetch output.
-    Args
-        fpath     (str)        : path to file to run `exiftool` on
-        rmtags    (str or list): name(s) of tags to remove with `exiftool`
-        attr_name (str or list): filter output exif dictionary by attribute name(s)
-    Returns
-        dict
-    """
-    import subprocess, re
-    from os.path import isfile
-    from pydoni.sh import syscmd
-    from pydoni.vb import echo
-    assert isfile(fpath)
-
-    # Check if `exiftool` is installed
-    ep = syscmd('which terminal-notifier').decode().strip()
-    if not isfile(ep):
-        echo("terminal-notifier is not installed! Please install it per instructions with `brew install exiftool`", abort=True)
-
-    # If rmtags is specified, then call to `exiftool` was initiated in order to remove tags
-    if rmtags is not None:
-        if isinstance(rmtags, str):
-            res = syscmd('exiftool -overwrite_original -{}= "{}"'.format(rmtags, fpath))
-        elif isinstance(rmtags, list):
-            for tag in rmtags:
-                res = syscmd('exiftool -overwrite_original -{}= "{}"'.format(tag, fpath))
-        else:
-            echo("Parameter 'rmtags' must be of type str or list", fn_name='exiftool', abort=True)
-        return None
-
-    # Normal call to this function
-    res = subprocess.run('exiftool "%s"' % fpath, stdout=subprocess.PIPE, shell=True)
-    res = str(res.stdout.decode('utf-8', errors='backslashreplace'))
-    exif = [x for x in res.split('\n') if x > '']
-    keys = [re.sub(r'^(.*?)(:)(.*)$', r'\1', x).strip() for x in exif]
-    vals = [re.sub(r'^(.*?)(:)(.*)$', r'\3', x).strip() for x in exif]
-    exif_dict = dict(zip(keys, vals))
-    exif_dict = {k.lower().replace(' ', '_'): v for k, v in exif_dict.items()}
-    
-    # Filter result if specified
-    if attr_name:
-        if isinstance(attr_name, str):
-            attr_name = [attr_name]
-        return {key: exif_dict[key] for key in attr_name}
-    else:
-        return exif_dict
-
-
 def adobe_dng_converter(fpath, overwrite=False):
     """
     Run Adobe DNG Converter on a file.
