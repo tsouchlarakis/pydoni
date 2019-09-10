b85b851489078ed1fa0480ea13597b6fc242c7c3
tsouchlarakis <Iloveandoni$44>
2019-09-10 15:20:52 -0700

    added naturalsort

--- pydoni/pyobj.py
+++ pydoni/pyobj.py
@@ -592,4 +592,31 @@ def markdown_toc(md_fpath, li_type):
     return '\n'.join(h_toc)
 
 
+def naturalsort(lst):
+    """
+    Sort a list with numeric elements, numerically.
+
+    Arguments:
+        lst {list} -- list to sort
+
+    Returns:
+        {list}
+
+    Source:
+        https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
+    """
+    def atoi(text):
+        return int(text) if text.isdigit() else text
+
+    def natural_keys(text):
+        '''
+        alist.sort(key=natural_keys) sorts in human order
+        http://nedbatchelder.com/blog/200712/human_sorting.html
+        (See Toothy's implementation in the comments)
+        '''
+        return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
+    return sorted(lst, key=natural_keys)
+
+
 from pydoni.vb import echo
