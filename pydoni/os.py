f6cfbf9191c3bf6b5f3e255de497ea5f01c223e2
tsouchlarakis <Iloveandoni$44>
2019-10-27 22:54:49 -0700

    added draw function

--- pydoni/os.py
+++ pydoni/os.py
@@ -13,6 +13,9 @@ from os.path import expanduser
 from os.path import join
 from os.path import basename
 from os.path import splitext
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
 
 
 class FinderMacOS(object):
@@ -483,6 +486,137 @@ def find_drives():
     volumes = listdirs(join('/', 'Volumes'), full_names=True)
     volumes = [v for v in volumes if basename(v) != 'AndoniHD']
     return volumes
+
+
+def make_meme(
+    image_file,
+    output_file,
+    msg,
+    msg_color='white',
+    msg_pos='bottom',
+    msg_outline_color='black',
+    msg_outline_width=3,
+    font_path='/System/Library/Fonts/HelveticaNeue.ttc',
+    font_size=200,
+    repel_from_edge=0.0
+    ):
+    """
+    Draw text on an image file.
+
+    Arguments:
+        image_file {str} -- image file to draw on
+        output_file {str} -- filepath to save output image to
+        msg {str} -- message to write on image
+
+    Keyword Arguments
+        msg_color {str} -- color of message (default: {'white'})
+        msg_pos {str} -- position of message, only 'bottom' supported (default: {'bottom'})
+        msg_outline_color {str} -- message outline color (default: {'black'})
+        msg_outline_width {int} -- width of text outline (default: {3})
+        font_path {str} -- path to font to use {default: '/System/Library/Fonts/HelveticaNeue.ttc'}
+        font_size {int} -- desired font size of `msg`
+        repel_from_edge {float} -- shift text position this % of the image height/width away from the edge if text position is an edge of the image. Example: if the specified position is top-left, the text will be printed right up against the top and left edges. If a value of 0.05 is specified for `repel_from_edge`, then the text will be shifted down 5% of the image height and shifted right 5% of the image width (default: {0.0})
+    """
+
+    img = Image.open(image_file)
+    draw = ImageDraw.Draw(img)
+
+    lines = []
+
+    font = ImageFont.truetype(font_path, font_size)
+    w, h = draw.textsize(msg, font)
+
+    imgWidthWithPadding = img.width * 0.99
+
+    # 1. how many lines for the msg to fit ?
+    lineCount = 1
+    if w > imgWidthWithPadding:
+        lineCount = int(round((w / imgWidthWithPadding) + 1))
+
+    if lineCount > 2:
+        while 1:
+            font_size -= 2
+            font = ImageFont.truetype(font_path, font_size)
+            w, h = draw.textsize(msg, font)
+            lineCount = int(round((w / imgWidthWithPadding) + 1))
+            if lineCount < 3 or font_size < 10:
+                break
+
+    # If msg contains no spaces but is long enough to justify multiple
+    # lines, revert to lineCount = 1 as the next part will fail, attempting
+    # to split the message into multiple lines on a space
+    if lineCount > 1 and ' ' not in msg:
+        lineCount = 1
+
+    # 2. divide text in X lines
+    lastCut = 0
+    isLast = False
+    for i in range(0,lineCount):
+        if lastCut == 0:
+            cut = (len(msg) / lineCount) * i
+        else:
+            cut = lastCut
+
+        if i < lineCount-1:
+            nextCut = (len(msg) / lineCount) * (i+1)
+        else:
+            nextCut = len(msg)
+            isLast = True
+
+        # make sure we don't cut words in half
+        nextCut = int(nextCut)
+        nextCutOriginal = nextCut
+        if nextCut == len(msg) or msg[nextCut] == " ":
+            pass
+        else:
+            # Check forward and backward from nextCut to get position of
+            # string to cut at (look for nearest whitespace)
+            while msg[nextCut] != " ":
+                if nextCut == len(msg)-1:
+                    nextCut = nextCutOriginal
+                    break
+                nextCut += 1
+
+            while msg[nextCut] != " ":
+                nextCut -= 1
+
+        cut = int(cut)
+        line = msg[cut:nextCut].strip()
+
+        # is line still fitting ?
+        w, h = draw.textsize(line, font)
+        if not isLast and w > imgWidthWithPadding:
+            nextCut -= 1
+            while msg[nextCut] != " ":
+                nextCut -= 1
+
+        lastCut = nextCut
+        lines.append(msg[cut:nextCut].strip())
+
+
+    lastY = -h
+    if msg_pos == "bottom":
+        repel_pixels = repel_from_edge * img.height
+        repel_pixels = int(repel_pixels)
+        lastY = img.height - h * (lineCount+1) - repel_pixels
+
+    for i in range(0,lineCount):
+        w, h = draw.textsize(lines[i], font)
+        textX = img.width/2 - w/2
+        textY = lastY + h
+        
+        mow = msg_outline_width
+        moc = msg_outline_color
+        if moc is not None:
+            draw.text((textX-mow, textY-mow),lines[i],moc,font=font)
+            draw.text((textX+mow, textY-mow),lines[i],moc,font=font)
+            draw.text((textX+mow, textY+mow),lines[i],moc,font=font)
+            draw.text((textX-mow, textY+mow),lines[i],moc,font=font)
+
+        draw.text((textX, textY),lines[i],msg_color,font=font)
+        lastY = textY
+
+    img.save(output_file)
 
 
 from pydoni.db import Postgres
